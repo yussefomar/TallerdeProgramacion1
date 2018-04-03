8afac5241e8af213a3bdf4a5e34c33f92580f4d1
supernovac28 <cm.fernandez.28@gmail.com>
2018-04-03 15:01:46 -0300

    parser

---
+++ Utils/Util_Parser.h
@@ -0,0 +1,20 @@
+#ifndef UTIL_PARSER_H_
+#define UTIL_PARSER_H_
+
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+class Util_Parser
+{
+	public:
+
+		void deleteFile();
+        int getLogLevel();
+        int getFormacion();
+        int getCasaca();
+};
+
+#endif
