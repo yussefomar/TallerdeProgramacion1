69a284e7bcd1db5c99ee35c4dea2c3379a8c4c93
Matias Gutierrez <matiasgutierrez@outlook.com>
2018-04-15 22:23:09 -0300

    bug fix 2

--- Model/Activo.cpp
+++ Model/Activo.cpp
@@ -1,7 +1,7 @@
 #include "Activo.h"
 
-#define ANCHO_JUGADOR 45
-#define ALTO_JUGADOR 45
+#define ANCHO_JUGADOR 62
+#define ALTO_JUGADOR 62
 //dimension del nivel
 #define ANCHO_NIVEL 2048
 #define ALTO_NIVEL 1318
@@ -19,7 +19,7 @@ Activo::~Activo()
 void Activo::move() {
     this->mPosX += this->mVelX;
     //If the dot went too far to the left or right
-    if( ( mPosX < 0 ) || ( mPosX + ANCHO_JUGADOR > ANCHO_NIVEL) )
+    if( ( mPosX < 0 ) || ( mPosX + ANCHO_JUGADOR >= ANCHO_NIVEL) )
     {
         //Move back
         mPosX -= mVelX;
@@ -28,7 +28,7 @@ void Activo::move() {
     //Move the dot up or down
     mPosY += mVelY;
     //If the dot went too far up or down
-    if( ( mPosY < 0 ) || ( mPosY + ALTO_JUGADOR > ALTO_NIVEL ) )
+    if( ( mPosY < 0 ) || ( mPosY + ALTO_JUGADOR >= ALTO_NIVEL ) )
     {
         //Move back
         mPosY -= mVelY;
